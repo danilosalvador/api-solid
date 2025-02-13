051e7274a3a4009df1c30f18072705dfc92b3807
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2025-02-13 14:10:57 -0300

    Hash da senha e validação

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
 import { z } from 'zod'
+
 import { prisma } from '@/lib/prisma'
 
 export async function register(
@@ -13,12 +15,21 @@ export async function register(
     })
 
     const { name, email, password } = registerBodySchema.parse(request.body)
+    const password_hash = await hash(password, 8)
+
+    const emailExists = await prisma.user.findUnique({
+        where: { email },
+    })
+
+    if (emailExists) {
+        return response.status(409).send()
+    }
 
     await prisma.user.create({
         data: {
             name,
             email,
-            password_hash: password,
+            password_hash,
         },
     })
 
