a4330b164ce39477e406298bc1b9d3331d9aa77a
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2025-02-14 15:05:37 -0300

    Primeiro teste unitário

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,27 @@
-import { expect, test } from 'vitest'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { expect, describe, it } from 'vitest'
+import { UserRegisterService } from './register'
+import { compare } from 'bcryptjs'
 
-test('register', () => {
-    expect(2 + 2).toBe(4)
+describe('Register Use Case', () => {
+    it('should hash user password upon registration', async () => {
+        // Arrange
+        const prismaUsersRepository = new PrismaUsersRepository()
+
+        const userRegisterService = new UserRegisterService(
+            prismaUsersRepository
+        )
+
+        const password = '123456'
+
+        const { user } = await userRegisterService.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password,
+        })
+
+        const isPasswordHashed = await compare(password, user.password_hash)
+
+        expect(isPasswordHashed).toBe(true)
+    })
 })
--- src/services/register.ts
+++ src/services/register.ts
@@ -2,6 +2,7 @@ import { hash } from 'bcryptjs'
 
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
 import type { UsersRepository } from '@/repositories/users-repository'
+import type { User } from '@prisma/client'
 
 interface RegisterServiceParams {
     name: string
@@ -9,10 +10,18 @@ interface RegisterServiceParams {
     password: string
 }
 
+interface RegisterServiceResponse {
+    user: User
+}
+
 export class UserRegisterService {
     constructor(private usersRepository: UsersRepository) {}
 
-    async execute({ name, email, password }: RegisterServiceParams) {
+    async execute({
+        name,
+        email,
+        password,
+    }: RegisterServiceParams): Promise<RegisterServiceResponse> {
         const password_hash = await hash(password, 8)
 
         const emailExists = await this.usersRepository.findByEmail(email)
@@ -21,11 +30,14 @@ export class UserRegisterService {
             throw new UserAlreadyExistsError()
         }
 
-        // const prismaUsersRepository = new PrismaUsersRepository()
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password_hash,
         })
+
+        return {
+            user,
+        }
     }
 }
