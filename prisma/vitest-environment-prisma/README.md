b46ddb24e0774aabf1007146fefc8e092c6645d6
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2025-02-26 18:14:26 -0300

    Criando test environment

---
+++ prisma/vitest-environment-prisma/README.md
@@ -0,0 +1,4 @@
+Para configurar o isolamento dos testes E2E:
+
+- Na pasta "prisma/vitest-environment-prisma" executar no terminal: ```npm link```
+- Na raiz do projeto executar no terminal ```npm link vitest-environment-prisma```
---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,15 @@
+import type { Environment } from 'vitest'
+
+export default (<Environment>{
+    name: 'prisma',
+    transformMode: 'ssr',
+    async setup() {
+        console.log('setup')
+
+        return {
+            teardown() {
+                console.log('teardown')
+            },
+        }
+    },
+})
