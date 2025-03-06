de64e94fff9e8cd5f72c11cba8d02e67ad8f1994
Danilo Salvador <danilo.salvador@smartlogic.com.br>
2025-03-06 18:21:34 -0300

    Testes de RBAC

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,7 +1,21 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
+export async function createAndAuthenticateUser(
+    app: FastifyInstance,
+    isAdmin = false
+) {
+    await prisma.user.create({
+        data: {
+            name: 'Danilo Salvador',
+            email: 'danilo.salvador@test.com',
+            password_hash: await hash('123@321', 8),
+            role: isAdmin ? 'ADMIN' : 'MEMBER',
+        },
+    })
+
     await request(app.server).post('/users').send({
         name: 'Danilo Salvador',
         email: 'danilo.salvador@test.com',
